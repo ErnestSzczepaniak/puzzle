9541f6420d5e9d678326426bb753c18e15f2ef6f
en2 <ernest.szczepaniak.en2@gmail.com>
2020-02-25 11:05:34 +0100

    usuniecie cmake, dodanie makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -std=c++1z main.cpp source/display.cpp source/game.cpp source/item_generic.cpp source/item_square.cpp -Iinclude/ -lX11 -o puzzle.elf
--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
-#ifdef build_platform_host
-
-#define CATCH_CONFIG_MAIN
-#include "build.h"
-#include "test.h"
-
-#else
-
-#include "build.h"
+#include "game.h"
 
 int main()
 {
+    Game game(4);
+
+    while(1)
+    {
+        auto key = d_input_wait();
+
+        if (key == 82) game.react(Key::UP);
+        else if (key == 84) game.react(Key::DOWN);
+        else if (key == 81) game.react(Key::LEFT);
+        else if (key == 83) game.react(Key::RIGHT);
+
+        if (game.end()) break;
+    }
+
+    game.info();
 
     while(1);
 }
 
-#endif
