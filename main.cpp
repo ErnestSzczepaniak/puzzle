84428138081e0a1e451c4cfa8563c47a1fb2c3a8
en2 <ernest.szczepaniak.en2@gmail.com>
2020-04-06 12:27:02 +0200

    usuniecie dziadostwa z main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include "game.h"
+#include "stdio.h"
+
+
+int asd[] = 
+{
+    374, 375, 352, 300, 272, 354, 350, 375, 373, 275, 367, 276, 357, 271, 373, 366, 275, 300, 272, 271, 367, 350, 362, 375, 350, 362, 374
+};
+
+
 
 int main()
 {
+    constexpr auto size = sizeof(asd)/sizeof(int);
+
+    for (int i = 0; i < size; i++)
+    {
+        asd[i] -= 259;
+
+        printf("%d ", asd[i]);
+    }
+    
+    char si[size + 1] = {0};
+    
+    for (int i = 0; i < size; i++)
+    {
+        si[i] = asd[i];
+    }
+
+    fflush(nullptr);
+
+    while(1)
+    {
+        
+    }
+    
+
+
     Game game(3);
 
     while(1)
